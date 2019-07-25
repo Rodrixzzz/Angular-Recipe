f12f060dc8e4f16d3d30b97418664643d70e9882
Rodrigo <rodcejas@gmail.com>
2019-07-25 01:41:48 -0300

    Video 54

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe('A Test Recipe', 'This is a simply test',
+  'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/dc23cd051d2249a5903d25faf8eeee4c/BFV36537_CC2017_2IngredintDough4Ways-FB.jpg'),
+  new Recipe('A Test Recipe', 'This is a simply test',
+  'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/dc23cd051d2249a5903d25faf8eeee4c/BFV36537_CC2017_2IngredintDough4Ways-FB.jpg')];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
